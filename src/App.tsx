3ab778ba2a744cde56b4e830fdb8ad749388aec7
null <wasimthoufiq21@gmail.com>
2022-04-16 17:11:02 +0530

    deployed and UI changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import Alert from "./components/Alert";
 import DBank from "./components/DBank";
 import { StoreProvider } from "./store/context";
 
 export default function App() {
+  useEffect(()=>{
+    document.title = "DBank Defi app"
+  },[])
   return (
     <StoreProvider>
       <Alert />
